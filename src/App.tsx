465cd8f30a12c1cd9497cbbbd66bd034b232274c
rafaelspa <rsdepaula67@gmail.com>
2023-01-17 14:51:05 -0300

    Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import Home from "./components/Home";
+
 export default function App() {
-  return (<h1>App</h1>)
+  return (
+    <Home/>
+  );
 }
---
+++ src/components/Home/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+export default function Home() {
+
+    return (
+        <header className="dsf-header">
+            <div className="dsf-header-container">
+                <h1 className="dsf-header-title">DSFilter</h1>
+                <div className="dsf-header-product-count">6 produto(s)</div>
+            </div>
+        </header>
+    )
+}
